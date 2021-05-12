4f6a34583226327175b43507c32cee29cdbf833a
Purushottam Kumar <kpuru2@gmail.com>
2021-05-12 10:05:17 +0530

    updated table scripts

--- SQL DB Scripts/Tables/Payments.sql
+++ SQL DB Scripts/Tables/Payments.sql
@@ -4,7 +4,7 @@ Id BIGINT NOT NULL,
 TaxYear INT NOT NULL,
 AccountNumber VARCHAR(100) NOT NULL,
 Status VARCHAR(100) NOT NULL,
-Amount DECIMAL NOT NULL,
+Amount DECIMAL(10,2),
 PaidDate DATETIME,
 CONSTRAINT PK_Payments_Id PRIMARY KEY (Id),
 CONSTRAINT FK_Payments_AccountNumber FOREIGN KEY (AccountNumber) REFERENCES Accounts (Number)
